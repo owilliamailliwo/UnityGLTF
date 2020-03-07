fa1a1df1df819b15cbd9d12f30e7aa04ea577d22
William <owilliamailliwo@gmail.com>
2020-03-07 19:07:32 +0800

    Change algorithm.

--- UnityGLTF/Assets/UnityGLTF/Runtime/Scripts/AnimationCorrector.cs
+++ UnityGLTF/Assets/UnityGLTF/Runtime/Scripts/AnimationCorrector.cs
@@ -9,7 +9,6 @@ using GLTF.Schema;
 public class AnimationCorrector
 {
 	public static string DealtWithBoneName = "root";
-	private string DealtWithCurveBindingsPath;
 
 	public static bool DisableYMovement = false;
 	public static bool DisableXZMovement = true;
@@ -18,11 +17,13 @@ public class AnimationCorrector
 	private static string LeftFootName = FootName + "_L";
 	private static string RightFootName = FootName + "_R";
 
+	private string _dealtWithCurveBindingsPath;
 	private string _leftFootCurveBindingsPath;
 	private List<EditorCurveBinding> _leftFootCurveBindings;
 	private string _rightFootCurveBindingsPath;
 	private List<EditorCurveBinding> _rightFootCurveBindings;
 
+	private List<GLTFSceneExporter.CurveBindingGroup> _dealtWithBoneParentCurveBindings = new List<GLTFSceneExporter.CurveBindingGroup>();
 	private List<GLTFSceneExporter.CurveBindingGroup> _leftFootParentCurveBindings = new List<GLTFSceneExporter.CurveBindingGroup>();
 	private List<GLTFSceneExporter.CurveBindingGroup> _rightFootParentCurveBindings = new List<GLTFSceneExporter.CurveBindingGroup>();
 
@@ -31,7 +32,6 @@ public class AnimationCorrector
 
 	public void Init(List<GLTFSceneExporter.CurveBindingGroup> curveBindingGroups, Transform rootNodeTransform)
 	{
-		Transform parentBone = null;
 		Transform leftFootBone = null;
 		Transform rightFootBone = null;
 
@@ -73,22 +73,22 @@ public class AnimationCorrector
 
 			if (bone.name == DealtWithBoneName)
 			{
-				DealtWithCurveBindingsPath = curveBindingGroup.path;
-				parentBone = bone.parent;
+				_dealtWithCurveBindingsPath = curveBindingGroup.path;
 			}
 		}
 
-		if (null == leftFootBone || null == rightFootBone || null == parentBone)
+		if (null == leftFootBone || null == rightFootBone)
 		{
 			Debug.LogError("Invalid skeleton structure.");
 			return;
 		}
 
-		var leftFoot = parentBone.transform.worldToLocalMatrix.MultiplyPoint3x4(leftFootBone.position);
-		var rightFoot = parentBone.transform.worldToLocalMatrix.MultiplyPoint3x4(rightFootBone.position);
+		var leftFoot = rootNodeTransform.transform.worldToLocalMatrix.MultiplyPoint3x4(leftFootBone.position);
+		var rightFoot = rootNodeTransform.transform.worldToLocalMatrix.MultiplyPoint3x4(rightFootBone.position);
 
 		_initialPoint = (leftFoot + rightFoot) / 2.0f;
 
+		GetAllParents(_dealtWithCurveBindingsPath, ref _dealtWithBoneParentCurveBindings, curveBindingGroupsDict);
 		GetAllParents(_leftFootCurveBindingsPath, ref _leftFootParentCurveBindings, curveBindingGroupsDict);
 		GetAllParents(_rightFootCurveBindingsPath, ref _rightFootParentCurveBindings, curveBindingGroupsDict);
 	}
@@ -100,7 +100,7 @@ public class AnimationCorrector
 		string curveBindingGroupPath,
 		out GLTFAnimationChannelPath path)
 	{
-		if(curveBindingGroupPath != DealtWithCurveBindingsPath)
+		if(curveBindingGroupPath != _dealtWithCurveBindingsPath)
 		{
 			path = GLTFAnimationChannelPath.translation;
 			return null;
@@ -115,30 +115,24 @@ public class AnimationCorrector
 					{
 						var time = i / animationClip.frameRate;
 
-						var curveIndex = 0;
-						var value = new float[3];
-						foreach (var curveBinding in curveBindings)
-						{
-							var curve = AnimationUtility.GetEditorCurve(animationClip, curveBinding);
-							if (curve != null) value[curveIndex++] = curve.Evaluate(time);
-						}
-
-						var frameData = new Vector3(value[0], value[1], value[2]);
+						Vector3 frameData = Vector3.zero;
+						GetPostionInWorldSpace(time, animationClip, curveBindings, _dealtWithBoneParentCurveBindings, ref frameData);
 
 						Vector3 left = Vector3.zero;
 						Vector3 right = Vector3.zero;
-						GetPostionInParentSpace(time, animationClip, _leftFootCurveBindings, _leftFootParentCurveBindings, ref left);
-						GetPostionInParentSpace(time, animationClip, _rightFootCurveBindings, _rightFootParentCurveBindings, ref right);
+						GetPostionInWorldSpace(time, animationClip, _leftFootCurveBindings, _leftFootParentCurveBindings, ref left);
+						GetPostionInWorldSpace(time, animationClip, _rightFootCurveBindings, _rightFootParentCurveBindings, ref right);
 
 						Vector3 newPoint = (left + right) / 2.0f;
 						Vector3 offset = newPoint - _initialPoint;
+						offset = new Vector3(DisableXZMovement ? offset.x : 0.0f,
+							DisableYMovement ? offset.y : 0.0f,
+							DisableXZMovement ? offset.z : 0.0f);
 
 						var corrFrameData = frameData - offset;
+						GetPostionInParentSpace(time, animationClip, curveBindings, _dealtWithBoneParentCurveBindings, ref corrFrameData);
 
-						// TODO Notice what Y means, Y should be the Y direction in world space.
-						data[i] = new Vector3(DisableXZMovement ? corrFrameData.x : frameData.x,
-							DisableYMovement ? corrFrameData.y : frameData.y,
-							DisableXZMovement ? corrFrameData.z : frameData.z);
+						data[i] = corrFrameData;
 					}
 
 					path = GLTFAnimationChannelPath.translation;
@@ -165,23 +159,23 @@ public class AnimationCorrector
 		return null;
 	}
 
-	private void GetPostionInParentSpace(float time,
+	private void GetPostionInWorldSpace(float time,
 		AnimationClip animationClip,
-		List<EditorCurveBinding> footCurveBindings,
-		List<GLTFSceneExporter.CurveBindingGroup> footParentCurveBindings,
-		ref Vector3 footPos)
+		List<EditorCurveBinding> curveBindings,
+		List<GLTFSceneExporter.CurveBindingGroup> parentCurveBindings,
+		ref Vector3 position)
 	{
 		var curveIndex = 0;
 		var value = new float[3];
-		foreach (var curveBinding in footCurveBindings)
+		foreach (var curveBinding in curveBindings)
 		{
 			var curve = AnimationUtility.GetEditorCurve(animationClip, curveBinding);
 			if (curve != null) value[curveIndex++] = curve.Evaluate(time);
 		}
 
-		footPos = new Vector3(value[0], value[1], value[2]);
+		position = new Vector3(value[0], value[1], value[2]);
 
-		foreach (var curveBindingGroup in footParentCurveBindings)
+		foreach (var curveBindingGroup in parentCurveBindings)
 		{
 			Vector3 translation = Vector3.zero;
 			Quaternion quaternion = Quaternion.identity;
@@ -234,23 +228,83 @@ public class AnimationCorrector
 			}
 
 			Matrix4x4 m = Matrix4x4.TRS(translation, quaternion.normalized, scale);
-			footPos = m.MultiplyPoint3x4(footPos);
+			position = m.MultiplyPoint3x4(position);
 		}
 	}
 
-	private void GetAllParents(string footCurveBindingsPath,
-		ref List<GLTFSceneExporter.CurveBindingGroup> footParentCurveBindings,
+	private void GetPostionInParentSpace(float time,
+		AnimationClip animationClip,
+		List<EditorCurveBinding> curveBindings,
+		List<GLTFSceneExporter.CurveBindingGroup> parentCurveBindings,
+		ref Vector3 postion)
+	{
+		for (int i = parentCurveBindings.Count - 1; 0 <= i; --i)
+		{
+			Vector3 translation = Vector3.zero;
+			Quaternion quaternion = Quaternion.identity;
+			Vector3 scale = Vector3.one;
+
+			var curveBindingGroup = parentCurveBindings[i];
+
+			foreach (var property in curveBindingGroup.properties)
+			{
+				switch (property.name)
+				{
+					case "m_LocalPosition":
+						{
+							var ci = 0;
+							var v = new float[3];
+							foreach (var curveBinding in property.curveBindings)
+							{
+								var curve = AnimationUtility.GetEditorCurve(animationClip, curveBinding);
+								if (curve != null) v[ci++] = curve.Evaluate(time);
+							}
+
+							translation = new Vector3(v[0], v[1], v[2]);
+							break;
+						}
+					case "m_LocalRotation":
+						{
+							var ci = 0;
+							var v = new float[4];
+							foreach (var curveBinding in property.curveBindings)
+							{
+								var curve = AnimationUtility.GetEditorCurve(animationClip, curveBinding);
+								if (curve != null) v[ci++] = curve.Evaluate(time);
+							}
+
+							quaternion = new Quaternion(v[0], v[1], v[2], v[3]);
+							break;
+						}
+					case "m_LocalScale":
+						{
+							var ci = 0;
+							var v = new float[3];
+							foreach (var curveBinding in property.curveBindings)
+							{
+								var curve = AnimationUtility.GetEditorCurve(animationClip, curveBinding);
+								if (curve != null) v[ci++] = curve.Evaluate(time);
+							}
+
+							scale = new Vector3(v[0], v[1], v[2]);
+							break;
+						}
+				}
+			}
+
+			Matrix4x4 m = Matrix4x4.TRS(translation, quaternion.normalized, scale);
+			postion = m.inverse.MultiplyPoint3x4(postion);
+		}
+	}
+	
+	private void GetAllParents(string curveBindingsPath,
+		ref List<GLTFSceneExporter.CurveBindingGroup> parentCurveBindings,
 		Dictionary<string, GLTFSceneExporter.CurveBindingGroup> curveBindingGroups)
 	{
-		var path = footCurveBindingsPath;
+		var path = curveBindingsPath;
 		var removeStart = path.Length;
 		while (removeStart != -1)
 		{
-			if (path == DealtWithCurveBindingsPath)
-			{
-				break;
-			}
-
 			removeStart = path.LastIndexOf("/");
 			if (removeStart != -1)
 			{
@@ -259,7 +313,7 @@ public class AnimationCorrector
 				GLTFSceneExporter.CurveBindingGroup parent;
 				if (curveBindingGroups.TryGetValue(path, out parent))
 				{
-					footParentCurveBindings.Add(parent);
+					parentCurveBindings.Add(parent);
 				}
 			}
 		}
